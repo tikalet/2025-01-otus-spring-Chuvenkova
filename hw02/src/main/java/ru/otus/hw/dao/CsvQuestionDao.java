8da12d4ef34f13b71bbe783a0211a1d6454049c2
tikale <ladytegira@yandex.ru>
2025-03-01 12:56:39 +0300

    удален неиспользуемый импорт

--- hw02/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw02/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -9,7 +9,6 @@ import ru.otus.hw.domain.Question;
 import ru.otus.hw.exceptions.QuestionReadException;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
