c8d884030adf04ee3bd5c6358310e5d8adc6dfce
tikale <ladytegira@yandex.ru>
2025-03-26 21:56:53 +0300

    add homework 06

---
+++ hw06/src/test/java/ru/otus/hw/repositories/JpaAuthorRepositoryTest.java
@@ -0,0 +1,51 @@
+package ru.otus.hw.repositories;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
+import ru.otus.hw.models.Author;
+import ru.otus.hw.models.Book;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Репозиторий на основе JPA для работы с авторами ")
+@DataJpaTest
+@Import({JpaAuthorRepository.class})
+public class JpaAuthorRepositoryTest {
+
+    private static final long FIRST_ID = 1;
+
+    private static final int AUTHORS_SIZE = 3;
+
+    @Autowired
+    private JpaAuthorRepository repositoryJpa;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @DisplayName("должен загружать список всех авторов")
+    @Test
+    void shouldReturnCorrectAuthorsList() {
+        var actualAuthors = repositoryJpa.findAll();
+
+        assertThat(actualAuthors).isNotNull().hasSize(AUTHORS_SIZE)
+                .allMatch(author -> author.getFullName() != null && !author.getFullName().isEmpty());
+        actualAuthors.forEach(System.out::println);
+    }
+
+    @DisplayName("должен загружать автора по id")
+    @Test
+    void shouldReturnCorrectAuthorById() {
+        var actualAuthor = repositoryJpa.findById(FIRST_ID);
+        var expectedAuthor = entityManager.find(Author.class, FIRST_ID);
+        assertThat(actualAuthor).isPresent()
+                .get()
+                .isEqualTo(expectedAuthor);
+    }
+}
